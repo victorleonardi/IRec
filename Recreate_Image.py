fc4a7a03deb06916fe5ccdf94c6a9eccc0b05291
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2021-07-10 03:04:46 -0300

    Estudo da composição das cores nas imagens selecionadas.

--- Recreate_Image.py
+++ Recreate_Image.py
@@ -12,9 +12,51 @@ image = io.imread(fname=image_file)
 # plt.show()
 time.sleep(5)
 
-histogram, bin_edges = np.histogram(image, bins=256, range=(0, 255))
+# colors = ('red', 'green', 'blue')
+# channel_ids = (0, 1, 2)
+# plt.xlim([0, 256])
 
-plt.figure()
-plt.title("Histograma")
-plt.plot(bin_edges[0:-1], histogram)
+# for channel_id, c in zip(channel_ids, colors):
+#     histogram, bin_edges = np.histogram(
+#         image[:, :, channel_id], bins=256, range=(0, 256)
+#     )
+#     plt.plot(bin_edges[0:-1], histogram, color=c)
+
+# a sintaxe '...' desempacota tudo até o elemento final, dado por 0 aqui.
+red = image[..., 0]
+green = image[..., 1]
+blue = image[..., 2]
+
+fig, axs = plt.subplots(2, 2)
+
+cax_00 = axs[0, 0].imshow(image)
+axs[0, 0].xaxis.set_major_formatter(plt.NullFormatter())  # kill xlabels
+axs[0, 0].yaxis.set_major_formatter(plt.NullFormatter())  # kill ylabels
+
+cax_01 = axs[0, 1].imshow(red, cmap='Reds')
+fig.colorbar(cax_01, ax=axs[0, 1])
+axs[0, 1].xaxis.set_major_formatter(plt.NullFormatter())
+axs[0, 1].yaxis.set_major_formatter(plt.NullFormatter())
+
+cax_10 = axs[1, 0].imshow(green, cmap='Greens')
+fig.colorbar(cax_10, ax=axs[1, 0])
+axs[1, 0].xaxis.set_major_formatter(plt.NullFormatter())
+axs[1, 0].yaxis.set_major_formatter(plt.NullFormatter())
+
+cax_11 = axs[1, 1].imshow(blue, cmap='Blues')
+fig.colorbar(cax_11, ax=axs[1, 1])
+axs[1, 1].xaxis.set_major_formatter(plt.NullFormatter())
+axs[1, 1].yaxis.set_major_formatter(plt.NullFormatter())
+plt.show()
+
+# Plot histograms
+fig, axs = plt.subplots(3, sharex=True, sharey=True)
+
+axs[0].hist(red.ravel(), bins=10)
+axs[0].set_title('Red')
+axs[1].hist(green.ravel(), bins=10)
+axs[1].set_title('Green')
+axs[2].hist(blue.ravel(), bins=10)
+axs[2].set_title('Blue')
+
 plt.show()
